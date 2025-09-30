a340d8cfb2a62d9f81160478cc21bb4a700d3491
Abdullah Al Roman <abdullahr583@live.com>
2025-09-30 21:41:30 +0600

    change routes

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,9 @@ import 'package:get/get.dart';
 void main() {
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: '/welcome',
+    initialRoute: '/',
     getPages: [
-      GetPage(name: '/welcome', page: () => WelcomeScreen()),
+      GetPage(name: '/', page: () => WelcomeScreen()),
       GetPage(name: '/categoriesScreen', page: () => CategoriesScreen(), binding: QuizCategoriesBinding()),
       GetPage(name: '/quiz_configuration_screen', page: () => QuizConfigurationScreen(selectedCategoryId: Get.arguments as int), binding: QuizCategoriesBinding()),
       GetPage(name: '/single_question_screen', page: () => SingleQuestionScreen(quesitonList: Get.arguments as List<AllQuestionModel>), binding: SingleQuestionBinding()),
